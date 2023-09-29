67bf268ced5e09bba279a78855499abe880dd742
Richard Mario Raun <ricky.raun@gmail.com>
2023-09-30 01:12:31 +0300

    1. commit

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
-print("Tere")
-print('tomat')
-print('kartul')
+path = '/Users/ricky.raun/Documents/GitHub/Gcode-M106-controll/testmaterial/30cube_0.2mm_ABS_39m.txt'
+fail = open(path, encoding = "UTF-8")
+
+realugeja=0
+for rida in fail:
+    realugeja+=1
+    rida = rida.strip("\n").split(":")
+    for sone in rida:
+        if sone == "Overhang perimeter":
+            print(f"rida: {realugeja} ja sone on {sone}")
+
+
+
+fail.close()
+
