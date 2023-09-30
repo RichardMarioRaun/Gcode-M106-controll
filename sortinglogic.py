6af3669f21d5a96b6a4bcecbcba5beac54f27668
JanMarkusBachmann <bachmannjanmarkus@gmail.com>
2023-09-30 17:07:27 +0300

    sortlogic

---
+++ sortinglogic.py
@@ -0,0 +1,4 @@
+# def see peaks lopuks olema funktsioon millega sorteerimine toimub
+sisse = ('G1 F2000\nG1 X100 Y100\n;TYPE:Perimeter\nM106 S128\nG1 X300 Y300 E.1\nM107\n;TYPE:External Perimeter') #See on praegu lambine sisend mis voib hiljem gcodeist tulla
+#print(sisse)
+
