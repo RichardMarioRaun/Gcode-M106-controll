08f3c18d9055d2a6c42a07f5fe91b8272f2b4182
JanMarkusBachmann <bachmannjanmarkus@gmail.com>
2023-10-05 00:29:20 +0300

    jannubranch update tootav

--- sortinglogic.py
+++ sortinglogic.py
@@ -1,4 +1,15 @@
-# def see peaks lopuks olema funktsioon millega sorteerimine toimub
-sisse = ('G1 F2000\nG1 X100 Y100\n;TYPE:Perimeter\nM106 S128\nG1 X300 Y300 E.1\nM107\n;TYPE:External Perimeter') #See on praegu lambine sisend mis voib hiljem gcodeist tulla
-#print(sisse)
+path = r"C:\Users\Jan Markus\Documents\GitHub\Gcode-M106-controll\testmaterial\karlireket_0.2mm_PETG_8h55m.gcode"
+fail = open(path, encoding = "UTF-8")
 
+txt = []
+typenr = set()
+
+for rida in fail:
+    txt.append(rida.strip('\n'))
+fail.close()
+
+for rida in txt:
+    if rida.find(';TYPE:') == 0:
+        typenr.add(str(rida[6:]))
+
+print(typenr)
