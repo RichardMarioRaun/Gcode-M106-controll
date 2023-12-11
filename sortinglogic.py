dccbef28a6d4dc99df816ec96d56ef0441bed8de
JanMarkusBachmann <bachmannjanmarkus@gmail.com>
2023-12-11 11:46:58 +0200

    jannubranch update tootav zcord search

--- sortinglogic.py
+++ sortinglogic.py
@@ -1,16 +1,24 @@
+import customtkinter
 import tkinter.filedialog
-path = tkinter.filedialog.askopenfile()
-fail = open(path, encoding = "UTF-8")
 
-txt = []
-typenr = set()
+def confwizzardstart():
+    print("button clicked")
 
-for rida in fail:
-    txt.append(rida.strip('\n'))
-fail.close()
+def editorstart():
+    print('m106edit')
 
-for rida in txt:
-    if rida.find(';TYPE:') == 0:
-        typenr.add(str(rida[6:]))
+app = customtkinter.CTk()
+app.geometry("400x200")
 
-print('\n'.join(typenr))
+teatetekst = customtkinter.CTkLabel(app, text="Mis sa teha soovid?", fg_color="transparent")
+teatetekst.pack(padx=20, pady=20)
+
+editornupp = customtkinter.CTkButton(app, text="Moondata Gcode faili valmis seadetega", fg_color=("#DB3E39", "#821D1A"), command=editorstart())
+#button = customtkinter.CTkButton(app, fg_color=("#DB3E39", "#821D1A"))  # tuple color
+editornupp.pack(padx=20, pady=10)
+
+confwizzardnupp = customtkinter.CTkButton(app, text="Teha uued seaded", command=confwizzardstart())
+#button = customtkinter.CTkButton(app, fg_color=("#DB3E39", "#821D1A"))  # tuple color
+confwizzardnupp.pack(padx=20, pady=10)
+
+app.mainloop()
