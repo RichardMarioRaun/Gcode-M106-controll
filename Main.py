c10a52e1530addf004d44e60631710a50c2e75a0
JanMarkusBachmann <bachmannjanmarkus@gmail.com>
2023-12-21 01:25:01 +0200

    jannubranch update tootav zcord search

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 import tkinter.filedialog
 import editor as BE
 
-class App(ctk.CTk):
+class Mainwin(ctk.CTk):
     def __init__(self, name, geom, **kwargs):
         ctk.CTk.__init__(self,**kwargs)
         self.title(name)
@@ -11,18 +11,13 @@ class App(ctk.CTk):
         self.logpath = ''
         self.exitpath = ''
         self.internal = []
-        introtxt = ctk.CTkLabel(self, text='Mis sa teha soovid?')
-        self.internal.append(introtxt)
-        editinitB = ctk.CTkButton(self, text='Moondata Gcode faili valmis seadetega', command=self.editbutton)
-        self.internal.append(editinitB)
-        confwizB = ctk.CTkButton(self, text='Teha uued seaded', command=self.confwiz)
-        self.internal.append(confwizB)
-        self.repack()
-
-
         self.sorceN = PathselectFr(self, 'Vali alusfail')
         self.logN = PathselectFr(self, 'Vali seadete CSV fail')
         self.exitN = PathselectFr(self, 'Vali väljundi fail')
+        self.editbutton()
+
+
+
     def repack(self):
         for asi in self.internal:
             asi.pack(pady=10)
@@ -40,8 +35,8 @@ class App(ctk.CTk):
         self.internal.append(introtxt)
         editinitB = ctk.CTkButton(self, text='Moondata Gcode faili valmis seadetega', command=self.editbutton)
         self.internal.append(editinitB)
-        confwizB = ctk.CTkButton(self, text='Teha uued seaded', command=self.confwiz)
-        self.internal.append(confwizB)
+        #confwizB = ctk.CTkButton(self, text='Teha uued seaded', command=self.confwiz)
+        #self.internal.append(confwizB)
         self.repack()
     def editbutton(self):
         self.depack()
@@ -51,8 +46,8 @@ class App(ctk.CTk):
         self.internal.append(self.logN)
         self.internal.append(self.exitN)
         loppvalik = ctk.CTkFrame(self)
-        tagasiN = ctk.CTkButton(loppvalik, text='tagasi...', command=self.restore)
-        tagasiN.pack(padx=10, side='left')
+        #tagasiN = ctk.CTkButton(loppvalik, text='tagasi...', command=self.restore)
+        #tagasiN.pack(padx=10, side='left')
         startN = ctk.CTkButton(loppvalik, text='START', command=self.editstart)
         startN.pack(padx=10, side='left')
         self.internal.append(loppvalik)
@@ -81,9 +76,7 @@ class App(ctk.CTk):
             self.after(1000, self.editbutton)
             #if valmis in self.internal:
                 #self.internal.remove(valmis)
-    def confwiz(self):
-        self.depack()
-        self.repack()
+
 class PathselectFr(ctk.CTkFrame):
     def __init__(self, master, note, **kwargs):
         super().__init__(master, **kwargs)
@@ -103,43 +96,6 @@ class PathselectFr(ctk.CTkFrame):
     def getpath(self):
         return self.path
 
-class Conftabel(ctk.CTkFrame):
-    jarg = ['Vahemik', 'External perimeter', 'Perimeter', 'Overhang perimeter', 'Internal infill', 'Top solid infill', 'Solid infill', 'Support material interface', 'Support material', 'Skirt/Brim,Bridge infill']
-    def __init__(self, master, logipath=None, **kwargs):
-        super().__init__(master, **kwargs)
-        self.logipath = logipath
-        self.conftxt = []
-    def readconf(self):
-        logi = BE.Conf(self.logipath)
-        self.conftxt = []
-        for kiht in logi.kihid:
-            RN = []
-            RN.append(kiht.start)
-            Rn.append(kiht.end)
-            RN.append(BE.deconvert(kiht.externalperimeter))
-            RN.append(BE.deconvert(kiht.perimeter))
-            RN.append(BE.deconvert(kiht.overhangperimeter))
-            RN.append(BE.deconvert(kiht.internalinfill))
-            RN.append(BE.deconvert(kiht.topsolidinfill))
-            RN.append(BE.deconvert(kiht.solidinfill))
-            RN.append(BE.deconvert(kiht.supportmaterialinterface))
-            RN.append(BE.deconvert(kiht.supportmaterial))
-            RN.append(BE.deconvert(kiht.skirtbrim))
-            RN.append(BE.deconvert(kiht.bridgeinfill))
-            self.conftxt.append(RN)
 
-    def confrend(self):
-
-class confcolm(ctk.CTkFrame):
-    def __init__(self, master, defa=True, discr, values, **kwargs):
-        super().(master, **kwargs)
-        self.tyyp=defa
-        self.discr=discr
-        self.values=values
-        self.sisu = []
-
-    def ty1in(self):
-
-
-win = App('M106 editor', '800x300')
+win = Mainwin('M106 editor', '800x300')
 win.mainloop()
--- objektid.py
+++ objektid.py
@@ -1,45 +1,242 @@
 import customtkinter as ctk
 import tkinter.filedialog
+import editor as BE
 
-class Window(ctk.CTk):
-    def __init__(self, geom, name, **kwargs):
+
+class Mainwin(ctk.CTk):
+    def __init__(self, name, geom, **kwargs):
         ctk.CTk.__init__(self, **kwargs)
-        self.geometry(geom)
         self.title(name)
-        self.obj = []
+        self.geometry(geom)
+        self.sorcepath = ''
+        self.logpath = ''
+        self.exitpath = ''
+        self.internal = []
+        introtxt = ctk.CTkLabel(self, text='Mis sa teha soovid?')
+        self.internal.append(introtxt)
+        editinitB = ctk.CTkButton(self, text='Moondata Gcode faili valmis seadetega', command=self.editbutton)
+        self.internal.append(editinitB)
+        confwizB = ctk.CTkButton(self, text='Teha uued seaded', command=self.confwiz)
+        self.internal.append(confwizB)
+        self.repack()
+
+        self.sorceN = PathselectFr(self, 'Vali alusfail')
+        self.logN = PathselectFr(self, 'Vali seadete CSV fail')
+        self.exitN = PathselectFr(self, 'Vali väljundi fail')
+
+    def repack(self):
+        for asi in self.internal:
+            asi.pack(pady=10)
+
+    def depack(self):
+        for asi in self.internal:
+            asi.pack_forget()
+        self.internal = []
+
+    def restore(self):
+        self.depack()
+        self.sorcepath = ''
+        self.logpath = ''
+        self.exitpath = ''
+        introtxt = ctk.CTkLabel(self, text='Mis sa teha soovid?')
+        self.internal.append(introtxt)
+        editinitB = ctk.CTkButton(self, text='Moondata Gcode faili valmis seadetega', command=self.editbutton)
+        self.internal.append(editinitB)
+        confwizB = ctk.CTkButton(self, text='Teha uued seaded', command=self.confwiz)
+        self.internal.append(confwizB)
+        self.repack()
+
+    def editbutton(self):
+        self.depack()
+        exp = ctk.CTkLabel(self, text='Vali vastavad failid ja vajuta start')
+        self.internal.append(exp)
+        self.internal.append(self.sorceN)
+        self.internal.append(self.logN)
+        self.internal.append(self.exitN)
+        loppvalik = ctk.CTkFrame(self)
+        tagasiN = ctk.CTkButton(loppvalik, text='tagasi...', command=self.restore)
+        tagasiN.pack(padx=10, side='left')
+        startN = ctk.CTkButton(loppvalik, text='START', command=self.editstart)
+        startN.pack(padx=10, side='left')
+        self.internal.append(loppvalik)
+        self.repack()
+
+    def editstart(self):
+        self.sorcepath = self.sorceN.getpath()
+        self.logpath = self.logN.getpath()
+        self.exitpath = self.exitN.getpath()
+        valmis = ctk.CTkLabel(self, text='Valmis!')
+        viga = ctk.CTkLabel(self, text='Kõik failid pole valitud')
+
+        if len(self.internal) == 6:
+            (self.internal[-1]).pack_forget()
+            self.internal.remove(self.internal[-1])
+
+        if self.sorcepath and self.logpath and self.exitpath:
+            BE.director(self.sorcepath, self.logpath, self.exitpath)
+            self.internal.append(valmis)
+            valmis.pack(pady=10)
+
+            self.after(1000, self.editbutton)
+
+        else:
+            self.internal.append(viga)
+            viga.pack(pady=10)
+            self.after(1000, self.editbutton)
+            # if valmis in self.internal:
+            # self.internal.remove(valmis)
+
+    def confwiz(self):
+        self.depack()
+        self.repack()
 
 
-    def addframe(self, tingi):
-        self.obj.append(tingi)
-        for asi in self.obj:
-            asi.pack(padx=10, pady=10)
+class PathselectFr(ctk.CTkFrame):
+    def __init__(self, master, note, **kwargs):
+        super().__init__(master, **kwargs)
+        self.path = ''
+        self.txt = ctk.CTkLabel(self, text=note)
+        self.txt.pack(padx=10, side='left')
+        self.B1 = ctk.CTkButton(self, text='Vajuta siia ja vali fail', command=self.pathselect)
+        self.B1.pack(padx=10, side='left')
 
-    def killframe(self):
-        self.obj = []
+    def pathselect(self):
+        pathnimi = str(tkinter.filedialog.askopenfile())
+        self.path = pathnimi.split("'")[1]
+        line = self.path.split('/')[-1]
+        self.B1.configure(text=line)
 
-class Nupp(ctk.CTkButton):
-    sorcepath = ''
-    logpath = ''
-    exitpath = ''
+    def restorename(self):
+        self.B1.configure(text='Vajuta siia ja vali fail')
 
-    def __init__(self, origin, txt, cmd=None):
-        ctk.CTkButton.__init__(self, master=origin, text=txt, command=cmd)
+    def getpath(self):
+        return self.path
 
-class Editpress(Nupp):
-    def __init__(self, origin, txt):
-        Nupp.__init__(self, origin, txt, cmd=self.startedit)
-        self.origin = origin
 
-    def startedit(self):
-        self.configure(text='vajuta veel')
-        self.origin.killinit()
-        print('tere')
+class Confwiztabl(ctk.CTkFrame):
+    def __init__(self, master, logipath=None, defal=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.logipath = logipath
+        self.conftxt = []
+        self.tablinternal = []
+        self.defal = defal
+        self.defffa = [['-', '-'], [self.defal], [self.defal], [self.defal], [self.defal], [self.defal], [self.defal],
+                       [self.defal], [self.defal], [self.defal]]
+        self.jarg = ('External perimeter', 'Perimeter', 'Overhang perimeter', 'Internal infill', 'Top solid infill',
+                     'Solid infill', 'Support material interface', 'Support material', 'Skirt/Brim,Bridge infill')
+        if self.logipath != None:
+            defakys = ctk.CTkInputDialog(text='Vali oma tavaline õguvoolu protsernt', title="Default")
+            self.defal = defakys.get_input()
 
-def killinit(frame):
-    origin.after(1000, lambda: print('tere'))
-    frame.killframe()
+    def readconf(self):
+        logi = BE.Conf(self.logipath)
+        self.conftxt = []
+        for kiht in logi.kihid:
+            RN = []
+            RNd = []
+            RNd.append(kiht.start)
+            RNd.append(kiht.end)
+            RN.append(RNd)
+            RN.append(BE.deconvert(kiht.externalperimeter))
+            RN.append(BE.deconvert(kiht.perimeter))
+            RN.append(BE.deconvert(kiht.overhangperimeter))
+            RN.append(BE.deconvert(kiht.internalinfill))
+            RN.append(BE.deconvert(kiht.topsolidinfill))
+            RN.append(BE.deconvert(kiht.solidinfill))
+            RN.append(BE.deconvert(kiht.supportmaterialinterface))
+            RN.append(BE.deconvert(kiht.supportmaterial))
+            RN.append(BE.deconvert(kiht.skirtbrim))
+            RN.append(BE.deconvert(kiht.bridgeinfill))
+            self.conftxt.append(RN)
 
-r = Window('800x800', 'testnimi')
-n1 = Editpress(r, 'homho')
-r.addframe(n1)
-r.mainloop()
+    def confrend(self):
+        self.depack
+        rangeframe = Confrangecolm(self)
+        self.tablinternal.append(rangeframe)
+        for tyyp in self.jarg:
+            frame =
+        if self.logipath != None:
+            self.readconf()
+            for kiht in self.conftxt
+
+    def depack(self):
+        for asi in self.tablinternal:
+            asi.pack_forget()
+
+    def repack(self):
+        for asi in self.tablinternal:
+            asi.pack(padx=5, side='left')
+
+
+class Confrangecolm(ctk.CTkFrame):
+    def __init__(self, master, values=[['-', '-']], **kwargs):
+        super().__init__(master, **kwargs)
+        self.values = values  # list kiht[], kiht[], kiht[value, value]
+        self.sisu = []
+        self.default()
+
+    def default(self):
+        self.depack()
+        dis = ctk.CTkLabel(self, text='Vahemik')
+        dis.pack(pady=5)
+        for kiht in self.values:
+            RN = Rangerow(self, kiht[0], kiht[1])
+            self.sisu.append(RN)
+        self.repack()
+
+    def repack(self):
+        for element in self.sisu:
+            element.pack(pady=5)
+
+    def depack(self):
+        for element in self.sisu:
+            element.pack_forget()
+        self.sisu = []
+
+
+class Rangerow(ctk.CTkFrame):
+    def __init__(self, master, alg=None, lopp=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.alg = alg
+        self.lopp = lopp
+        self.algEnt = ctk.CTkEntry(self, placeholder_text='Algus')
+        self.algEnt.pack(padx=5, side='left')
+        self.loppEnt = ctk.CTkEntry(self, placeholder_text='Lõpp')
+        self.algEnt.pack(padx=5, side='left')
+        if self.alg != None and self.lopp != None:
+            self.algEnt.insert(self.alg)
+            self.loppEnt.insert(self.lopp)
+
+
+class Confcolm(ctk.CTkFrame):
+    def __init__(self, master, discr, values, defa=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.discr = discr
+        self.values = values
+        self.sisu = []
+        self.defa = defa
+        self.deffa()
+
+    def deffa(self):
+        self.depack()
+        nimi = ctk.CTkLabel(self, text=self.discr)
+        nimi.pack(pady=5)
+        for val in self.values:
+            RN = ctk.CTkEntry(self)
+            if defa != None:
+                RN.insert(str(defa))
+            self.sisu.append(RN)
+        self.repack()
+
+    def depack(self):
+        for asi in self.sisu:
+            asi.pack_forget()
+        self.sisu = []
+
+    def repack(self):
+        for asi in self.sisu:
+            asi.pack(pady=5)
+
+
+win = Mainwin('M106 editor', '800x300')
+win.mainloop()
